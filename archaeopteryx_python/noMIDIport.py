4e8bfc11c898631cc0ccac4e5037a0f0240802db
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-01 08:00:53 +0100

    Update noMIDIport.py

--- archaeopteryx_python/noMIDIport.py
+++ archaeopteryx_python/noMIDIport.py
@@ -1,1 +1,7 @@
+from music21 import stream, instrument, note, duration, midi
+import random
 
+def create_midi_melody():
+    """
+    Crea una melodia MIDI senza necessità di una porta MIDI fisica
+    """
