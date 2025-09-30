d7d9ccc17079ef50d9260f947a13a19352df09bd
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-09-30 08:31:41 +0200

    Update request_get.py

--- Python_API/REST/request_get.py
+++ Python_API/REST/request_get.py
@@ -1,1 +1,7 @@
+import requests
+
+response = requests.get('https://jsonplaceholder.typicode.com/posts')
+print(response.json())
+Contenuto generato dall'intelligenza artificiale. Rivedi e usa con attenzione. Ulteriori informazioni su FAQ.
+Spiegazione: Questo esempio invia una richiesta GET a un endpoint REST e stampa la risposta JSON.
 
