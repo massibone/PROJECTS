7c4a4f23fb1fff9818d020c31d17277468c7a950
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-11-17 08:22:23 +0100

    Update models.py

--- Python_API/Dieta_app/models.py
+++ Python_API/Dieta_app/models.py
@@ -1,1 +1,31 @@
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+class Utente(Base):
+    __tablename__ = "utenti"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    eta = Column(Integer)
+    peso = Column(Float)
+    altezza = Column(Float)
+    obiettivo_calorico = Column(Integer)  # es. 2000 kcal
+
+class Pasto(Base):
+    __tablename__ = "pasti"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    calorie = Column(Integer)
+    proteine = Column(Float)
+    carboidrati = Column(Float)
+    grassi = Column(Float)
+    utente_id = Column(Integer, ForeignKey("utenti.id"))
+
+class ListaSpesa(Base):
+    __tablename__ = "liste_spesa"
+    id = Column(Integer, primary_key=True, index=True)
+    ingrediente = Column(String)
+    quantita = Column(String)
+    utente_id = Column(Integer, ForeignKey("utenti.id"))
+    data = Column(Date)
